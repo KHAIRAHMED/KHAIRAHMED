b11596c708aebeeb4243999b8bfd68452ea60ee1
K-A <khairahmed234@gmail.com>
2022-01-21 01:04:07 +0600

    edited build folder

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import { ThemeContext } from './Context';
 
 const Footer = () => {
@@ -7,7 +6,7 @@ const Footer = () => {
     const darkMode = theme.state.darkMode
     return (
         <div className="text-center pb-5">
-            <p className={darkMode?"text-white":"text-dark"}>Copyright @{new Date().getFullYear()} <Link to="https://github.com/KHAIRAHMED">KHAIR AHMED</Link> ||
+            <p className={darkMode?"text-white":"text-dark"}>Copyright @{new Date().getFullYear()} <a href="https://github.com/KHAIRAHMED" rel="noreferrer" target="_blank">KHAIR AHMED</a> ||
             All Rights Reserve
             
             </p>
